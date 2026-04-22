ea9f623cee95221f67410e58279f63e7c08490ed
Victor Nani <123387218+AristarhMD@users.noreply.github.com>
2026-04-22 10:32:19 +0300

    UI update
    Arrow change to up when clicked on the features and company

--- script.js
+++ script.js
@@ -21,18 +21,38 @@ function toggleHidden() {
 
 for (const subMenu of subMenus) {
   subMenu.addEventListener("click", () => {
+    //   If was clicked feature div
     if (subMenu.querySelector(".feature-context")) {
+      let arrowEl = subMenu.querySelector("#arrow-feature");
+
+      // Toggle visible class fot the company div
       !companyContextEl.classList.contains("not-visible")
         ? companyContextEl.classList.toggle("not-visible")
         : null;
+
+      // Toggle visible class fot the feature div
       featureContextEl.classList.toggle("not-visible");
+
+      // Change of the img
+      featureContextEl.classList.contains("not-visible")
+        ? (arrowEl.src = "images/icon-arrow-down.svg")
+        : (arrowEl.src = "images/icon-arrow-up.svg");
     } else {
+      //   If was clicked company div
+      let arrowEl = subMenu.querySelector("#arrow-company");
+
+      // Toggle visible class fot the feature div
       !featureContextEl.classList.contains("not-visible")
         ? featureContextEl.classList.toggle("not-visible")
         : null;
+
+      // Toggle visible class fot the company div
       companyContextEl.classList.toggle("not-visible");
+
+      // Change of the img
+      companyContextEl.classList.contains("not-visible")
+        ? (arrowEl.src = "images/icon-arrow-down.svg")
+        : (arrowEl.src = "images/icon-arrow-up.svg");
     }
   });
 }
-
-// featureContextEl.addEventListener("click", () => {});
